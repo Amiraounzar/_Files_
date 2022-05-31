6983c16b5df640bc10c9b21c359c5c937e845e22
Amiraounzar <105659537+Amiraounzar@users.noreply.github.com>
2022-05-31 15:19:46 -0500

    Add files via upload

---
+++ classwork_28.js
@@ -0,0 +1,52 @@
+// 1. Use the correct Array method to remove the first item of the fruits array.
+
+var fruits = ["Banana", "Orange", "Apple"];
+
+// ANSWER 1:
+
+// 2. Use the correct Array method to add "Kiwi" as the first item of the fruits array.
+
+// ANSWER 2:
+
+// 3. Use the correct Array method to add "Strawberry" and then "Pineapple" to the end of the fruits array.
+
+// ANSWER 3:
+
+// 4. Change the second element "Onion" to "Carrot" in the veggies array.
+
+var veggies = ["Lettuce", "Onion", "Tomato"];
+
+// ANSWER 4:
+
+// 5. Use the splice() method to remove "Red" and "Orange" from colors.
+
+var colors = ["Blue", "White", "Black", "Red", "Orange"];
+
+// ANSWER 5:
+
+// 6. Use the slice() method to copy the entire colors array and save it in a variable called colors_2.
+
+// ANSWER 6:
+
+// 7. Use the correct Array method to sort the subjects array alphabetically.
+
+var subjects = ["Math", "Science", "English", "History"];
+
+// ANSWER 7:
+
+// 8. Use the correct Array method to add "Gold" at the end of the array. 
+// Then add "Silver" at the beginning. 
+// Then change "Black" to "Brown". 
+// Then use the splice() method to delete "White".
+
+// ANSWER 8:
+
+// 9. Combine the following arrays:
+var first = ["One", "Two", "Three"];
+var second = ["Four", "Five", "Six"];
+
+// ANSWER 9:
+
+// 10. Add "One-Half" between "One" and "Two" in array first. 
+
+// ANSWER 10:
